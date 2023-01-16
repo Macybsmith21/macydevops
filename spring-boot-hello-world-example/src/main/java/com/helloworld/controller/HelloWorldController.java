8d05c4f6a962e63af7c051390ea5fcb42049c839
Macy Smith <Macy.smith21@yahoo.com>
2023-01-15 22:42:54 -0500

    changed webpage

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
+return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. You did it Macy!!!!</h1>";
 }
 }
