1bcc17a39b3fb9f55d4693357dc578625d2bb7b3
Macy Smith <Macy.smith21@yahoo.com>
2023-02-09 14:45:37 -0500

    deploying devops stack again for security testing

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://devopsdeploy-webappdeploymentbucket-6faoicplxrmp/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://macydevopsstack-webappdeploymentbucket-vdkc6rcdfrts/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
